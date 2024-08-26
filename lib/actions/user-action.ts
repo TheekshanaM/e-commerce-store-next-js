cec225cb7284d715927aef8ea8ddcabe164bb4ae
TheekshanaM <93.madushan@gmail.com>
2024-08-26 12:48:44 +0530

    password hashing

--- lib/actions/user-action.ts
+++ lib/actions/user-action.ts
@@ -2,6 +2,7 @@
 
 import { signUp } from "../services/user-service";
 import { IUser, TSignUpForm } from "../types/userType";
+import bcrypt from "bcrypt";
 
 interface actionResponse {
   success: boolean;
@@ -11,9 +12,12 @@ interface actionResponse {
 export async function signUpAction(
   formData: TSignUpForm
 ): Promise<actionResponse> {
+  const saltRounds = 10;
   let user: IUser = { ...formData, createdDate: new Date() };
 
   try {
+    user.password = await bcrypt.hash(user.password, saltRounds);
+
     const response = await signUp(user);
     return response;
   } catch (error) {
