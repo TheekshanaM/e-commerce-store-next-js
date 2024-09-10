47432508e92228eda0269883ee8462a8e3f44837
TheekshanaM <93.madushan@gmail.com>
2024-09-10 10:36:14 +0530

    modify formik wrapper

--- component/form/formik-wrapper/FormikWrapper.tsx
+++ component/form/formik-wrapper/FormikWrapper.tsx
@@ -3,16 +3,22 @@ import { ReactNode } from "react";
 
 interface IFormikWrapper<Values extends FormikValues> {
   initialValues: Values;
+  enableReinitialize?: boolean;
   children: ReactNode | ((props: FormikProps<Values>) => ReactNode);
 }
 
 // Make FormikWrapper a generic component
 export default function FormikWrapper<Values extends FormikValues>({
   initialValues,
+  enableReinitialize,
   children,
 }: IFormikWrapper<Values>) {
   return (
-    <Formik<Values> initialValues={initialValues} onSubmit={() => {}}>
+    <Formik<Values>
+      enableReinitialize
+      initialValues={initialValues}
+      onSubmit={() => {}}
+    >
       {children}
     </Formik>
   );
